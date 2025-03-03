9f1a88a36d367dfed7da858d22d124204e16be0a
Dmitry <dsterligov2004@mail.ru>
2025-03-03 11:04:37 +0300

    1 лаба по Разработка на Python

---
+++ third_course/sixth_semestr/network_applications_python/lab1/secondlevel.py
@@ -0,0 +1,38 @@
+import math
+
+# 8.	Вкладчик положил в банк 10 000 рублей под 8 % в месяц. Определить, через какое время сумма удвоится. 
+startMoney = 10000
+x = startMoney
+count = 0
+while x < startMoney * 2:
+    x *= 1.08
+    count += 1
+print(count)
+
+
+# 2.	Определить наибольшее значение сомножителя n, для которого произведение р = 1 · 4 · 7 ·...· n не превышает L = 30 000.
+def getadd3fact(x):
+    fact = 1;
+    for i in range(1, x + 1, 3):
+        fact *= i
+    return fact
+
+n = 1
+while getadd3fact(n) < 30000:
+    n += 1 
+print(n)
+
+
+
+# 1.	Вычислить сумму s = cos x + (cos 2x)/22 + ... + (cos nx)/n2 +.... Суммирование прекратить, когда очередной член суммы по модулю будет меньше ε = 0,0001.
+def getcosfunc(x):
+    return math.cos(2 * x)/(x ** 2)
+
+
+s = 0
+for n in range(1, 100):
+    x = getcosfunc(n)
+    s += x
+    if abs(x) < 0.0001:
+        print(n)
+        break
---
+++ third_course/sixth_semestr/network_applications_python/lab1/thirdlevel.py
@@ -0,0 +1,59 @@
+from functools import lru_cache
+import math
+
+def calculation(a, b, h, func1, func2, i = 0):
+    s = 0
+    for x in [x / 10.0 for x in range(int(a * 10), int(b * 10), int(h * 10))]:
+        print()
+        print(f"y = {func1(x)}")
+        s = 0
+        i = 0
+        while abs(func2(x, i)) > 0.0001:
+            tmp = func2(x, i)
+            s += tmp
+            i += 1
+        print(f"s = {s}\ti = {i}")
+    return s
+ 
+
+@lru_cache()
+def func1_s(x, i):
+    return (-1) ** i * x ** (2 * i) / math.factorial(2 * i)
+
+
+def func1_y(x):
+    return math.cos(x)
+
+
+calculation(0.1, 1, 0.1, func1_y, func1_s)
+
+
+@lru_cache()
+def func3_s(x, i):
+    return  math.cos(i * x) / math.factorial(i)
+
+
+def func3_y(x):
+    return math.e ** math.cos(x) * math.cos(math.sin(x))
+
+
+calculation(0.1, 1, 0.1, func3_y, func3_s, i=1)
+
+
+@lru_cache()
+def func4_s(x, i):
+    return (2 * i + 1) * x ** (2 * i) / math.factorial(i)
+
+
+def func4_y(x):
+    return (1 + 2 * x ** 2) * math.e ** (x ** 2)
+
+
+calculation(0.1, 1, 0.1, func4_y, func4_s)
+
+
+
+
+
+
+
